fcc4674e554282c5cac439054244b97815d96cda
zanetu <zanetu@gmail.com>
2016-04-20 23:18:48 -0400

    direct slickdeals v1.0: cdn.rawgit.com
    direct slickdeals v1.0: README.md
    gitignore

---
+++ direct_slickdeals/README.md
@@ -0,0 +1,6 @@
+# Direct Slickdeals
+**License:** [GPLv2](http://www.gnu.org/licenses/old-licenses/gpl-2.0.html) or later
+
+**Install:** [Stable](https://cdn.rawgit.com/zanetu/userscripts/v1.0/direct_slickdeals/direct_slickdeals.user.js)
+
+#### A userscript that removes Slickdeals redirects and automatically opens forum threads of frontpage deals. 
---
+++ direct_slickdeals/direct_slickdeals.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         Direct Slickdeals
+// @namespace    github.com/zanetu
+// @version      1.0
+// @description  Removes Slickdeals redirects and automatically opens forum threads of frontpage deals. 
+// @include      /^http\:\/\/slickdeals\.net\/[a-z]\/\d+/
+// @author       zanetu
+// @license      GPL version 2 or any later version; http://www.gnu.org/licenses/gpl-2.0.txt
+// @grant        none
+// @run-at       document-end
+// @noframes
+// ==/UserScript==
+
+;(function() {
+	//start of slickdeals frontpage deals
+	var threadContainer = document.getElementById('comments')
+	var thread = threadContainer && threadContainer.getElementsByTagName('a')[0]
+	if(thread && thread.href) {
+		location.href = thread.href
+		return
+	}
+	//end of slickdeals frontpage deals
+	for(var i = 0, link; link = document.links[i]; i++) {
+		var m = link.href.match(/\=(http(?:\%3A|\:)(?:\%2F|\/){2}.+?)(?=\&|$)/i)
+		if(m && m[1]) {
+			link.href = decodeURIComponent(m[1]);
+		}
+	}
+})()
